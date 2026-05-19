75a3403a78c3ffd5cb7d209a9225a4e9569fd52e
bgao554 <bgao554@connect.hkust-gz.edu.cn>
2026-05-19 09:38:21 +0000

    Deploying to gh-pages from @ bgao554/ilsca@6d3aaef6500712cc211e3cc3b921dbe35c2ad292 🚀

---
+++ _pages/about_gaoborui.md
@@ -0,0 +1,1 @@
+Mr. Borui GAO is a Ph.D. student in HKUST(GZ), starting from Jan 2026. 
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,7 +53,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "Members of the lab",
           section: "Navigation",
           handler: () => {
             window.location.href = "/ilsca/people/";
